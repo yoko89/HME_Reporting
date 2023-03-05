8b73ee3e53f979369817c42ff5efa428b2bd4d8c
yoko <aikin1989@gmail.com>
2023-03-05 22:04:09 +0400

    Add divider for settings

--- app/src/main/java/com/neklaway/hme_reporting/feature_settings/presentation/SettingsScreen.kt
+++ app/src/main/java/com/neklaway/hme_reporting/feature_settings/presentation/SettingsScreen.kt
@@ -1,4 +1,4 @@
-package com.neklaway.hme_reporting.feature_settings.presentation.settings
+package com.neklaway.hme_reporting.feature_settings.presentation
 
 import android.app.Activity.RESULT_OK
 import android.content.Intent
@@ -27,7 +27,6 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.work.*
 import com.neklaway.hme_reporting.common.presentation.common.component.Selector
 import com.neklaway.hme_reporting.common.ui.theme.HMEReportingTheme
-import com.neklaway.hme_reporting.feature_settings.presentation.SettingsViewModel
 import com.neklaway.hme_reporting.feature_signature.presentation.signature.SignatureScreen
 import com.neklaway.hme_reporting.utils.Constants
 import com.neklaway.hme_reporting.utils.NotificationPermissionRequest
