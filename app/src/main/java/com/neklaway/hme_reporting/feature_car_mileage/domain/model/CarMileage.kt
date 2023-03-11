44b88e8628cfa3c4982b09102ce8c4d23996251c
yoko <aikin1989@gmail.com>
2023-03-11 21:58:37 +0400

    Fix broken test Change calendar to date

--- app/src/main/java/com/neklaway/hme_reporting/feature_car_mileage/domain/model/CarMileage.kt
+++ app/src/main/java/com/neklaway/hme_reporting/feature_car_mileage/domain/model/CarMileage.kt
@@ -1,20 +1,20 @@
 package com.neklaway.hme_reporting.feature_car_mileage.domain.model
 
 import com.neklaway.hme_reporting.common.data.entity.CarMileageEntity
-import com.neklaway.hme_reporting.utils.DateAsLongSerializer
+import com.neklaway.hme_reporting.utils.CalendarAsLongSerializer
 import kotlinx.serialization.Serializable
 import java.util.*
 
 @Serializable
 data class CarMileage(
-    @Serializable(with = DateAsLongSerializer::class)
+    @Serializable(with = CalendarAsLongSerializer::class)
     val startDate: Calendar,
-    @Serializable(with = DateAsLongSerializer::class)
+    @Serializable(with = CalendarAsLongSerializer::class)
     val startTime: Calendar,
     val startMileage: Long,
-    @Serializable(with = DateAsLongSerializer::class)
+    @Serializable(with = CalendarAsLongSerializer::class)
     val endDate: Calendar,
-    @Serializable(with = DateAsLongSerializer::class)
+    @Serializable(with = CalendarAsLongSerializer::class)
     val endTime: Calendar,
     val endMileage: Long,
     val id: Long? = null,
--- app/src/main/java/com/neklaway/hme_reporting/feature_visa/domain/model/Visa.kt
+++ app/src/main/java/com/neklaway/hme_reporting/feature_visa/domain/model/Visa.kt
@@ -1,14 +1,14 @@
 package com.neklaway.hme_reporting.feature_visa.domain.model
 
 import com.neklaway.hme_reporting.common.data.entity.VisaEntity
-import com.neklaway.hme_reporting.utils.DateAsLongSerializer
+import com.neklaway.hme_reporting.utils.CalendarAsLongSerializer
 import kotlinx.serialization.Serializable
 import java.util.*
 
 @Serializable
 data class Visa(
     val country: String,
-    @Serializable(with = DateAsLongSerializer::class)
+    @Serializable(with = CalendarAsLongSerializer::class)
     val date: Calendar,
     val selected: Boolean = false,
     val id: Long? = null,
