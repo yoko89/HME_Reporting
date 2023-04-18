2d5f28e5f816be79cac243df8f016abc4f68ff0a
yoko <aikin1989@gmail.com>
2023-04-19 01:52:57 +0400

    Fix Dropdown press algorithm Correct Blue color theme

--- app/src/main/java/com/neklaway/hme_reporting/common/presentation/common/component/DropDown.kt
+++ app/src/main/java/com/neklaway/hme_reporting/common/presentation/common/component/DropDown.kt
@@ -2,16 +2,34 @@ package com.neklaway.hme_reporting.common.presentation.common.component
 
 import android.util.Log
 import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.*
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.PressInteraction
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material3.*
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -33,19 +51,32 @@ fun <T> DropDown(
     warning: Boolean = false,
     onSelect: (T) -> Unit,
 ) {
+    val dropDown = remember {
+        mutableStateOf(false)
+    }
+    val interactionSource = remember {
+        MutableInteractionSource()
+    }
+    LaunchedEffect(key1 = interactionSource) {
+        interactionSource.interactions.collect { interaction ->
+            when (interaction) {
+                is PressInteraction.Press -> dropDown.value = true
+            }
+        }
+    }
+
     Column(
         modifier = modifier.fillMaxWidth(),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        val dropDown = remember {
-            mutableStateOf(false)
-        }
+
         val infiniteTransition = rememberInfiniteTransition()
 
         OutlinedTextField(
             value = selectedValue,
             onValueChange = {},
+            interactionSource = interactionSource,
             label = { Text(text = label) },
             modifier = Modifier
                 .fillMaxWidth()
