2b6bd52754a360ba7b59939ce620ac6250513af6
yoko <aikin1989@gmail.com>
2023-05-26 09:05:30 +0400

    gradle migrate to kotlin

---
+++ app/build.gradle.kts
@@ -0,0 +1,122 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    id("dagger.hilt.android.plugin")
+    id("kotlinx-serialization")
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = ("com.neklaway.hmereporting")
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.neklaway.hmereporting"
+        minSdk = 29
+        targetSdk = 33
+        versionCode = 12
+        versionName = "2.1"
+
+        testInstrumentationRunner = ("androidx.test.runner.AndroidJUnitRunner")
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+
+        javaCompileOptions {
+            annotationProcessorOptions {
+                arguments += mapOf(
+                    "room.schemaLocation" to "$projectDir/schemas",
+                    "room.incremental" to "true"
+                )
+
+            }
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    kotlinOptions {
+        freeCompilerArgs = listOf(
+            ("-opt-in=androidx.compose.material3.ExperimentalMaterial3Api")
+        )
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = ("1.4.3")
+    }
+    packaging {
+        resources {
+            excludes.add("/META-INF/{AL2.0,LGPL2.1}")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    buildToolsVersion = "33.0.1"
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.compose.ui:ui:1.4.3")
+    implementation("androidx.compose.material3:material3:1.1.0")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.4.3")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.documentfile:documentfile:1.0.1")
+    implementation("androidx.core:core-ktx:1.10.1")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.4.3")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.4.3")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.4.3")
+
+    // Compose dependencies
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation("androidx.compose.runtime:runtime-livedata:1.4.3")
+    implementation("androidx.navigation:navigation-compose:2.5.3")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.1")
+
+    //Room Database
+    implementation("androidx.room:room-runtime:2.5.1")
+    kapt("androidx.room:room-compiler:2.5.1")
+    implementation("androidx.room:room-ktx:2.5.1")
+
+    // Kotlin coroutine
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+
+    //Dagger
+    implementation("com.google.dagger:hilt-android:2.46.1")
+    kapt("com.google.dagger:hilt-compiler:2.46.1")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+    kapt("androidx.hilt:hilt-compiler:1.0.0")
+
+    // DataStore
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+
+    //Material Icons
+    implementation("androidx.compose.material:material-icons-extended:1.4.3")
+
+    //Serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+
+    //WorkManager
+    implementation("androidx.work:work-runtime-ktx:2.8.1")
+    implementation("androidx.hilt:hilt-work:1.0.0")
+}
---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+    dependencies {
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.44")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.8.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
+    }
+
+
+}
+plugins {
+    id("com.android.application") version("8.0.2") apply false
+    id("com.android.library") version("8.0.2") apply false
+    id("com.google.dagger.hilt.android") version("2.44") apply false
+    id("org.jetbrains.kotlin.android") version("1.8.10") apply false
+}
+
+tasks.register("clean",Delete::class) {
+    delete(rootProject.buildDir)
+}
