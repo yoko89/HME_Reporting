04276f84b15b31598faf45a580b8916920d4ce2a
yoko <aikin1989@gmail.com>
2023-09-11 17:41:34 +0400

    upgrade gradle and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ buildscript {
 
     dependencies {
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.9.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.9.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
 
     }
 
@@ -18,8 +18,8 @@ plugins {
     id("com.android.application") version("8.1.1") apply false
     id("com.android.library") version("8.1.1") apply false
     id("com.google.dagger.hilt.android") version("2.44") apply false
-    id("org.jetbrains.kotlin.android") version("1.9.0") apply false
-    id("com.google.devtools.ksp") version ("1.9.0-1.0.12") apply false
+    id("org.jetbrains.kotlin.android") version("1.9.10") apply false
+    id("com.google.devtools.ksp") version ("1.9.10-1.0.13") apply false
 }
 
 tasks.register("clean",Delete::class) {
