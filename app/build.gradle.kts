1aa48b4bb2931808b35b735265e810d6d644ef01
yoko <aikin1989@gmail.com>
2023-09-11 10:19:15 +0400

    upgrade gradle and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,18 +5,17 @@ plugins {
     kotlin("android")
     id("dagger.hilt.android.plugin")
     id("kotlinx-serialization")
-    id("kotlin-kapt")
     id("com.google.devtools.ksp")
 }
 
 android {
     namespace = ("com.neklaway.hmereporting")
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.neklaway.hmereporting"
         minSdk = 29
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 21
         versionName = "3.0"
 
@@ -58,7 +57,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = ("1.4.3")
+        kotlinCompilerExtensionVersion = ("1.5.2")
     }
     packaging {
         resources {
@@ -76,27 +75,27 @@ android {
 
 dependencies {
 
-    platform("androidx.compose:compose-bom:2023.08.00")
-    implementation("androidx.core:core-ktx:1.10.1")
+    platform("androidx.compose:compose-bom:2023.09.00")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3:1.1.1")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.documentfile:documentfile:1.0.1")
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.12.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.4.3")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.4.3")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.1")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.5.1")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // Compose dependencies
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("androidx.compose.runtime:runtime-livedata")
-    implementation("androidx.navigation:navigation-compose:2.6.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.1")
+    implementation("androidx.navigation:navigation-compose:2.7.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
 
     //Room Database
     implementation("androidx.room:room-runtime:2.5.2")
@@ -105,19 +104,19 @@ dependencies {
 
     // Kotlin coroutine
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
 
     //Dagger
-    implementation("com.google.dagger:hilt-android:2.46.1")
-    kapt("com.google.dagger:hilt-compiler:2.46.1")
+    implementation("com.google.dagger:hilt-android:2.48")
+    ksp("com.google.dagger:hilt-compiler:2.48")
     implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
-    kapt("androidx.hilt:hilt-compiler:1.0.0")
+    ksp("androidx.hilt:hilt-compiler:1.0.0")
 
     // DataStore
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 
     //Material Icons
-    implementation("androidx.compose.material:material-icons-extended:1.4.3")
+    implementation("androidx.compose.material:material-icons-extended:1.5.1")
 
     //Serialization
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
@@ -128,10 +127,10 @@ dependencies {
 
 
     //Testing hilt
-    testImplementation("com.google.dagger:hilt-android-testing:2.46.1")
-    kaptTest("com.google.dagger:hilt-android-compiler:2.46.1")
-    androidTestImplementation("com.google.dagger:hilt-android-testing:2.46.1")
-    kaptAndroidTest("com.google.dagger:hilt-android-compiler:2.46.1")
+    testImplementation("com.google.dagger:hilt-android-testing:2.48")
+    kspTest("com.google.dagger:hilt-android-compiler:2.48")
+    androidTestImplementation("com.google.dagger:hilt-android-testing:2.48")
+    kspAndroidTest("com.google.dagger:hilt-android-compiler:2.48")
 
     //Robolectric
     testImplementation("junit:junit:4.13.2")
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,20 +6,20 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.44")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.8.10")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.9.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
 
     }
 
 
 }
 plugins {
-    id("com.android.application") version("8.1.0") apply false
-    id("com.android.library") version("8.1.0") apply false
+    id("com.android.application") version("8.1.1") apply false
+    id("com.android.library") version("8.1.1") apply false
     id("com.google.dagger.hilt.android") version("2.44") apply false
-    id("org.jetbrains.kotlin.android") version("1.8.10") apply false
-    id("com.google.devtools.ksp") version ("1.8.10-1.0.9") apply false
+    id("org.jetbrains.kotlin.android") version("1.9.0") apply false
+    id("com.google.devtools.ksp") version ("1.9.0-1.0.12") apply false
 }
 
 tasks.register("clean",Delete::class) {
