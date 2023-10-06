fe1c1589f17247a0a53d5642c3859f00ecb4b715
yoko <aikin1989@gmail.com>
2023-10-06 19:36:05 +0400

    gradle and dependencies upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,26 +75,26 @@ android {
 
 dependencies {
 
-    platform("androidx.compose:compose-bom:2023.09.00")
+    platform("androidx.compose:compose-bom:2023.10.00")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.material3:material3:1.1.1")
+    implementation("androidx.compose.material3:material3:1.1.2")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.activity:activity-compose:1.8.0")
     implementation("androidx.documentfile:documentfile:1.0.1")
     implementation("androidx.core:core-ktx:1.12.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.1")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.5.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.3")
+    debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // Compose dependencies
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("androidx.compose.runtime:runtime-livedata")
-    implementation("androidx.navigation:navigation-compose:2.7.2")
+    implementation("androidx.navigation:navigation-compose:2.7.4")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
 
     //Room Database
@@ -109,21 +109,21 @@ dependencies {
     //Dagger
     implementation("com.google.dagger:hilt-android:2.48")
     ksp("com.google.dagger:hilt-compiler:2.48")
-    implementation("androidx.hilt:hilt-navigation-compose:1.1.0-alpha01")
-    ksp("androidx.hilt:hilt-compiler:1.1.0-alpha01")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0-beta01")
+    ksp("androidx.hilt:hilt-compiler:1.1.0-beta01")
 
     // DataStore
     implementation("androidx.datastore:datastore-preferences:1.0.0")
 
     //Material Icons
-    implementation("androidx.compose.material:material-icons-extended:1.5.1")
+    implementation("androidx.compose.material:material-icons-extended:1.5.3")
 
     //Serialization
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
 
     //WorkManager
     implementation("androidx.work:work-runtime-ktx:2.8.1")
-    implementation("androidx.hilt:hilt-work:1.1.0-alpha01")
+    implementation("androidx.hilt:hilt-work:1.1.0-beta01")
 
 
     //Testing hilt
