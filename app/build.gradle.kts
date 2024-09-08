b2ec54fbee98d332afb1288d4155025a046d3a3f
yoko <aikin1989@gmail.com>
2024-09-08 13:10:00 +0400

    update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,10 @@
-
 plugins {
     id("com.android.application")
     kotlin("android")
     id("dagger.hilt.android.plugin")
     id("kotlinx-serialization")
     id("com.google.devtools.ksp")
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20"
 }
 
 android {
@@ -15,8 +15,8 @@ android {
         applicationId = "com.neklaway.hmereporting"
         minSdk = 29
         targetSdk = 34
-        versionCode = 24
-        versionName = "3.3"
+        versionCode = 25
+        versionName = "3.4"
 
         testInstrumentationRunner = ("androidx.test.runner.AndroidJUnitRunner")
         vectorDrawables {
@@ -51,7 +51,7 @@ android {
         buildConfig = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = ("1.5.8")
+        kotlinCompilerExtensionVersion = ("1.5.13")
     }
     packaging {
         resources {
@@ -67,29 +67,33 @@ android {
     buildToolsVersion = "34.0.0"
 }
 
+composeCompiler {
+    reportsDestination = layout.buildDirectory.dir("compose_compiler")
+}
+
 dependencies {
 
-    platform("androidx.compose:compose-bom:2024.01.00")
-    implementation("androidx.core:core-ktx:1.12.0")
+    platform("androidx.compose:compose-bom:2024.09.00")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.material3:material3:1.2.0-rc01")
+    implementation("androidx.compose.material3:material3:1.3.0")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.5")
+    implementation("androidx.activity:activity-compose:1.9.2")
     implementation("androidx.documentfile:documentfile:1.0.1")
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.6.0")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.0")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // Compose dependencies
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.5")
     implementation("androidx.compose.runtime:runtime-livedata")
-    implementation("androidx.navigation:navigation-compose:2.7.6")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
+    implementation("androidx.navigation:navigation-compose:2.8.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.5")
 
     //Room Database
     implementation("androidx.room:room-runtime:2.6.1")
@@ -97,27 +101,27 @@ dependencies {
     implementation("androidx.room:room-ktx:2.6.1")
 
     // Kotlin coroutine
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.5")
 
     //Dagger
-    implementation("com.google.dagger:hilt-android:2.50")
-    ksp("com.google.dagger:hilt-compiler:2.50")
-    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
-    ksp("androidx.hilt:hilt-compiler:1.1.0")
+    implementation("com.google.dagger:hilt-android:2.52")
+    ksp("com.google.dagger:hilt-compiler:2.52")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    ksp("androidx.hilt:hilt-compiler:1.2.0")
 
     // DataStore
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
 
     //Material Icons
-    implementation("androidx.compose.material:material-icons-extended:1.6.0")
+    implementation("androidx.compose.material:material-icons-extended:1.7.0")
 
     //Serialization
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
 
     //WorkManager
-    implementation("androidx.work:work-runtime-ktx:2.9.0")
-    implementation("androidx.hilt:hilt-work:1.1.0")
+    implementation("androidx.work:work-runtime-ktx:2.9.1")
+    implementation("androidx.hilt:hilt-work:1.2.0")
 
 
     //Testing hilt
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,21 +6,17 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.50")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.9.21")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.52")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:2.0.20")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.20")
     }
 
 
 }
 plugins {
-    id("com.android.application") version("8.2.2") apply false
-    id("com.android.library") version("8.2.2") apply false
+    id("com.android.application") version("8.5.2") apply false
+    id("com.android.library") version("8.5.2") apply false
     id("com.google.dagger.hilt.android") version("2.44") apply false
-    id("org.jetbrains.kotlin.android") version("1.9.22") apply false
-    id("com.google.devtools.ksp") version ("1.9.22-1.0.17") apply false
-}
-
-tasks.register("clean",Delete::class) {
-    delete(rootProject.buildDir)
+    id("org.jetbrains.kotlin.android") version("2.0.20") apply false
+    id("com.google.devtools.ksp") version ("2.0.20-1.0.25") apply false
 }
